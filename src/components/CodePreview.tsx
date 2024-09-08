2701306d4470cbb7d1ad322407d35021b70b4f3b
Isaac Adebayo <bytegenlabs@gmail.com>
2024-09-08 18:41:34 +0100

    add-bg

--- src/components/CodePreview.tsx
+++ src/components/CodePreview.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
 
 const CodePreview = () => {
   return (
     <>
         <div className="side preview">
+            <button className="button drag-up outline">
+              show display <FaChevronUp className='circular' />
+            </button>
             <p>
               Click <b>Run</b>
             </p>
